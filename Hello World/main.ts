0da7a974275943267049c28ea365dc409efd8af8
Athar Khan <168277335+atharkhan0@users.noreply.github.com>
2024-05-01 16:08:49 -0700

    Hello world

--- Hello World/main.ts
+++ Hello World/main.ts
@@ -1,1 +1,58 @@
+//2hello = 'athar'
+//console.log (`hello ${hello}, would you like to learn some python today`)
 
+
+//3let person_name = 'Aqib'
+//console.log(person_name.toLowerCase())
+//console.log(person_name.toUpperCase())
+//console.log(person_name.charAt(0).toLowerCase()+person_name.slice(2))
+
+
+//4console.log('Tonny Robbin Once Said, "setting goals is the first step in turning the invisble into the visible')
+//let famous_person = "Tonny Robin"
+//let quote ="setting goals is the first step in turning the invisble into the visible"
+//console.log(`${famous_person} once said, ${quote}`)
+
+//5let famous_person = "A person who never made a mistake never tried anything new";
+//let quote = 'Tonny Robbin';
+//console.log (`${famous_person}once said, ${quote}`);
+//console.log (quote);
+
+// let personName: string =`\n\t ATHAR ARSHaD \t\n`;
+// console.log(personName);
+// let stripped: string = personName.trim(); 
+// console.log(stripped);
+
+// console.log(5+3);
+// console.log(10-2);
+// console.log(4*2);
+// console.log(16/2);
+
+
+// let favouritenumber: number = 8;
+// console.log(`my favouritenumber is ${favouritenumber}`);
+
+
+//ATHAR .........
+//6-MARCH -2024 ........
+
+//9console.log(`A B C D E F G H I J K L M N O P Q R S T U V W X Y Z`);
+
+//ATHAR .........
+//6-MARCH-2024 ......
+//IN THIS PROGRAN WE WILL WRITE A-Z:
+
+//10console.log(`1 2 3 4 5 6 7 8 9 10`);
+
+//let friends: string[] = [`zia`, `ahsan`, `habib`,]
+// for (let i=0; i<friends.length; i++)
+//  console.log(friends[i]);
+
+
+
+
+
+let aname ='athar'
+let bname = 'khan'
+
+console.log ('hello')

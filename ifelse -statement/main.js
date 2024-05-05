0fcdd6d6fef6e5fb5ad68a404f6b4b692f44d396
Athar Khan <168277335+atharkhan0@users.noreply.github.com>
2024-05-05 13:32:45 -0700

    Add files via upload

---
+++ ifelse -statement/main.js
@@ -0,0 +1,11 @@
+var house = 'kamran';
+var number = 404;
+if (house == 'kamran')
+    if (number == 404)
+        console.log('hello, kamran you can come');
+    else {
+        console.log('number not match');
+    }
+else {
+    console.log("no you cant come");
+}
---
+++ ifelse -statement/main.ts
@@ -0,0 +1,16 @@
+ let house:string ='kamran';
+let number:number = 404 ;
+
+if (house == 'kamran')
+    if(number == 404)
+    
+    console.log ('hello, kamran you can come')
+
+    else {
+        console.log ('number not match')
+
+    }
+
+else{
+    console.log ("no you cant come")
+}

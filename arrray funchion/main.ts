80d9b1c90749caaeda9459ccbe17afea9667d863
Athar Khan <168277335+atharkhan0@users.noreply.github.com>
2024-05-08 13:18:56 -0700

    Add files via upload

---
+++ arrray funchion/main.ts
@@ -0,0 +1,18 @@
+//let fruits = ['apple','orange','banana']
+//console.log(fruits.pop())                      //>>>=pop last wala element remove kry ga
+//console.log(fruits)
+
+//console.log(fruits.push('kiwi'))               //>>>= push last mai element add kr dyga
+//console.log(fruits)
+
+//console.log(fruits.shift())                   //>>>=shift first element ko remove kr  dyga
+//console.log(fruits)
+
+//console.log(fruits.unshift('pineapple'))     //>>>=unshift first mai element add krta hai
+//console.log(fruits)
+
+//console.log(fruits.splice(1,0,'mango'))      //>>>=splice koi element kahi bhi add kry ga
+//console.log(fruits)
+ 
+//let myarray =fruits.slice(2)                 //>>>=slice jitny number add kry gy otni value khatm ho jaye gi
+//console.log (myarray)                       //>>>= slice onle wrok varible

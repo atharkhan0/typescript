0656e6e4402d4f7e03cb24e26425d0dbcc72fd5e
Athar Khan <168277335+atharkhan0@users.noreply.github.com>
2024-05-19 09:56:05 -0700

    Add files via upload

---
+++ Array Funchion/main.js
@@ -0,0 +1,17 @@
+// let fruits = ['apple','orange','banana']
+// console.log(fruits.pop())                      //>>>=pop last wala element remove kry ga
+// console.log(fruits)
+// console.log(fruits.push('kiwi'))               //>>>= push last mai element add kr dyga
+// console.log(fruits)
+// console.log(fruits.shift())                   //>>>=shift first element ko remove kr  dyga
+// console.log(fruits)
+// console.log(fruits.unshift('pineapple'))     //>>>=unshift first mai element add krta hai
+// console.log(fruits)
+//begin
+//end (count)
+// console.log(fruits.splice(1,0,'mango'))      //>>>= splice orignal array ko modify kry ga
+// console.log(fruits)
+//begin
+//end-1
+// let myarray =fruits.slice(2)                 //>>>=slice orignal array modify nhi krta  & slice fake array bana kr dekhata hai & slice ek number phely stop ho jata hai;
+// console.log (myarray)                      

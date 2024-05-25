1481dbd9a09eb0e3a3afe899979604b3d3131c5a
Athar Khan <168277335+atharkhan0@users.noreply.github.com>
2024-05-25 07:04:07 -0700

    Add files via upload

---
+++ For Loop/main.ts
@@ -0,0 +1,87 @@
+
+// for ( let i = 1;      i<=4;     i++){
+//     console.log(3)
+// }
+
+
+
+// for (var i = 1; i <= 6; i++) {
+//     console.log(4);
+// }
+
+
+
+
+// let cleanstcities =['islamabad','lahore',];
+// for (let i =0; i<=1; i++){
+//     if("islamabad" === cleanstcities [i]){
+//         console.log ("its one of the cleanstcities");
+//         break;
+//     }
+// }
+
+
+// let number = 2
+
+// for(let a= 1 ; a<=10 ; a++){
+//     console.log(`${number} * ${a} = ${number * a}`)
+// }
+
+// let i:number = 10
+// let a:string = ""
+// for (let s:number = 1; s<=i; s++){
+//     for(let c:number = 1; c<=s; c++){
+//         a+= "*"
+//     }
+//     a+= '\h'
+// }
+// console.log(a)
+
+
+
+
+// let number = 2
+
+//  for(let a= 1 ; a<=10 ; a++){
+//      console.log(`${number} * ${a} = ${number * a}`)
+//  }
+
+
+// let number = 10255
+// for (let i=1; i<=5; i++){
+//     console.log(`${number} * ${i} = ${number * i}`)
+// }
+
+
+
+// let twiter = 2
+
+// for (let table = 1; table<=10; table++){
+//     for (let number = 1; number<=table; number++){
+//         console.log(`${twiter} * ${table} = ${twiter * table }`)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//                2 *  1  =    2 
+
+
+
+
+
+
+
+
+
